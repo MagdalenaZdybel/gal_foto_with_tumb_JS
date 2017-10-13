9d85ac2987caaaf7e5fc8937642ad5bc9acc9f1e
Magda <magda@motta.com.pl>
2017-10-13 17:54:23 +0200

    secend commit

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function()
+{
+var imageForWath = document.getElementById("imageForWath");
+var image = new Image;
+
+imageForWath.appendChild(image);
+
+image.src = "img/foto0.jpg";
+
+};
