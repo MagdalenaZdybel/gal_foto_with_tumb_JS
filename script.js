8aaf1a21b1553e8d30e2fd113233e6d35466935e
Magda <magda@motta.com.pl>
2017-10-13 18:08:31 +0200

    finish

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ var image = new Image;
 
 imageForWath.appendChild(image);
 
-image.src = "img/foto0.jpg";
+var thumbnail = document.getElementsByClassName("thumbnail");
+
+var currentThumbnail = thumbnail[0];
+
+image.src = currentThumbnail.getAttribute("src");
+currentThumbnail.className += " current";
+
+for (var i = 0; i < thumbnail.length; i++)
+{
+    thumbnail[i].onmouseover = function() //lub po onclick
+    {
+       currentThumbnail.className = currentThumbnail.className.replace("current", "");
+       currentThumbnail = this;
+       currentThumbnail.className += " current";
+       
+       image.src = this.getAttribute("src");
+    };
+};
 
 };
